33ef6e12134492fc952e61ba1dae0b71fa81196a
KeerthiSR01 <keerthisr0102@gmail.com>
2026-07-04 19:54:46 +0530

    Updated

---
+++ SumOfNaturalNumber.java
@@ -0,0 +1,8 @@
+public class SumOfNaturalNumber {
+    public static void main(String[] args) {
+       int sum = 0;
+       for(int i=1;i<=50;i++){
+           sum = sum+i;
+       }System.out.println("Sum of first 50 natural number is "+sum);
+    }
+}
