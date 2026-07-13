38fe2ff1ca1a5ab40079e89937d29c4a5fb52f8f
KeerthiSR01 <keerthisr0102@gmail.com>
2026-07-13 12:15:08 +0530

    Updated

---
+++ AverageOfThree.java
@@ -0,0 +1,9 @@
+public class AverageOfThree {
+    public static void main(String[] args) {
+        int num1 = 45;
+        int num2 = 67;
+        int num3 = 78;
+        double result = (num1+num2+num3)/3;
+        System.out.println("Average of "+num1+" " +num2+" "+num3+" is "+result);
+    }
+}
