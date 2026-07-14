a8d7475137f3ce2c85fdc27232c6bad2795f9705
KeerthiSR01 <keerthisr0102@gmail.com>
2026-07-14 20:12:02 +0530

    updated

---
+++ FindSquare.java
@@ -0,0 +1,9 @@
+public class FindSquare {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i=0;i<=n;i++){
+            int sqrt = (int)Math.pow(i,2);
+            System.out.println(sqrt);
+        }
+    }
+}
