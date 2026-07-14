8c413f56b872c6e64e0cf949acc5f67ac9863888
KeerthiSR01 <keerthisr0102@gmail.com>
2026-07-14 20:09:13 +0530

    updated

---
+++ FindCube.java
@@ -0,0 +1,8 @@
+public class FindCube {
+    public static void main(String[] args) {
+        for(int i=0;i<=5;i++){
+            int cube = (int)Math.pow(i,3);
+            System.out.println(cube);
+        }
+    }
+}
