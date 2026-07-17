802f5a8db7863c8fea5f4299942918c65c3cb277
KeerthiSR01 <keerthisr0102@gmail.com>
2026-07-17 19:54:05 +0530

    Updated

---
+++ HarshadNumber.java
@@ -0,0 +1,17 @@
+public class HarshadNumber {
+    public static void main(String[] args) {
+        int num = 18;
+        int original = num;
+        int sum = 0;
+
+        while (num != 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        if (original % sum == 0)
+            System.out.println("Harshad Number");
+        else
+            System.out.println("Not Harshad Number");
+    }
+}
