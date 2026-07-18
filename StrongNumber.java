f7b2166493199278d4819c1c3da9c4086c2e4dd5
KeerthiSR01 <keerthisr0102@gmail.com>
2026-07-18 22:13:33 +0530

    Updated

---
+++ StrongNumber.java
@@ -0,0 +1,24 @@
+public class StrongNumber {
+    public static void main(String[] args) {
+        int num = 145;
+        int original = num;
+        int sum = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            int fact = 1;
+
+            for (int i = 1; i <= digit; i++) {
+                fact *= i;
+            }
+
+            sum += fact;
+            num /= 10;
+        }
+
+        if (sum == original)
+            System.out.println("Strong Number");
+        else
+            System.out.println("Not Strong Number");
+    }
+}
